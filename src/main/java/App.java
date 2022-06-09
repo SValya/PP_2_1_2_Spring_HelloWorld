15d843b982825692df6a2562dcbcf68b24eb7df6
SValya <Samohva19@yandex.ru>
2022-06-09 05:46:39 +0300

    initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        //HelloWorld
+        HelloWorld beanHelloWorld =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHelloWorld.getMessage());
+        HelloWorld secondBeanHelloWorld =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(secondBeanHelloWorld.getMessage());
+        System.out.println(beanHelloWorld == secondBeanHelloWorld);
+
+        //Cat
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getMessage());
+        Cat secondBeanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(secondBeanCat.getMessage());
+        System.out.println(beanCat == secondBeanCat);
     }
 }
